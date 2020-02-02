9fe571ee058161be462aa90b95724cefa237661b
JiaYXiao <blah_647@hotmail.com>
2020-02-02 00:17:02 -0500

    Updates

---
+++ precache-manifest.af6d3cb6985e28f231b76a3dab7dbbb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac2bcfb6f8b0d745072dce2a90a18d5",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "c63c498de11c33698a4a",
+    "url": "/react-gh-pages/static/css/main.4c811a35.chunk.css"
+  },
+  {
+    "revision": "3c1a759b175266ee383f",
+    "url": "/react-gh-pages/static/js/2.a59bbf8f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-gh-pages/static/js/2.a59bbf8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c63c498de11c33698a4a",
+    "url": "/react-gh-pages/static/js/main.4ae2ce8c.chunk.js"
+  },
+  {
+    "revision": "748ed9e6ece805ab3155",
+    "url": "/react-gh-pages/static/js/runtime-main.e1e59220.js"
+  }
+]);
